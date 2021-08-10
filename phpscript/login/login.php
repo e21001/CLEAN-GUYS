8916e285267b99fae9a11fd6843b6de1860a0fca
Ryuya Asato <e21001@std.it-college.ac.jp>
2021-08-10 19:23:58 +0900

    修正

--- phpscript/login/login.php
+++ phpscript/login/login.php
@@ -3,15 +3,12 @@
   require_once dirname(__FILE__) . '/../dbconnect.php';
   session_start();
 
-<<<<<<< HEAD
   if ($_COOKIE['email'] != '') {
     $_POST['email'] = $_COOKIE['email'];
     $_POST['password'] = $_COOKIE['password'];
     $_POST['save'] = 'on';
   }
 
-=======
->>>>>>> d49694f263d82196fff26894f62740dba052a646
   if (!empty($_POST)) {
     // ログインの処理
     if ($_POST['email'] != '' && $_POST['password'] != '') {
@@ -27,15 +24,12 @@
         $_SESSION['id'] = $user['id'];
         $_SESSION['time'] = time();
 
-<<<<<<< HEAD
         // ログイン情報を記録する
         if ($_POST['save'] === 'on') {
           setcookie('email', $_POST['email'], time() + 60 * 60 * 24 * 14);
           setcookie('password', $_POST['password'], time() + 60 * 60 * 24 * 14);
         }
 
-=======
->>>>>>> d49694f263d82196fff26894f62740dba052a646
         header('Location: index.php');
         exit();
       } else {
