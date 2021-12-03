4958cea8a42cc978800ce6420fc2653916c67f6b
e21001 <e21001@std.it-college.ac.jp>
2021-12-03 17:49:29 +0900

    composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6aaebecea3ca7051f6b0e1ade03dd4963c519b48',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6aaebecea3ca7051f6b0e1ade03dd4963c519b48',
+            'dev_requirement' => false,
+        ),
+    ),
+);
